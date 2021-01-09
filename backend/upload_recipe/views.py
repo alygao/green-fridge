bcf621e4f1b93682e7471aaa428396af114065b1
alyssagao1120 <alyssa.gao@yahoo.ca>
2021-01-09 12:55:05 -0500

    added just additional rest methods

--- backend/upload_recipe/views.py
+++ backend/upload_recipe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from django.http import Http404
 from rest_framework import viewsets  
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -15,54 +15,65 @@ from PIL import Image
 
 
 # Create your views here.
-class FridgeFoodView(viewsets.ModelViewSet):       
-  serializer_class = FridgeFoodSerializer          
-  queryset = FridgeFood.objects.all()  
+class ExpiredFridgeFoodView(APIView): 
 
-  # parser_classes = (MultiPartParser, FormParser)
-
-  # def get(self, request, *args, **kwargs):
-  #     fridgeFoods = FridgeFood.objects.all()
-  #     serializer = FridgeFoodSerializer(fridgeFoods, many=True)
-  #     return Response(serializer.data)
-
-  # def post(self, request, *args, **kwargs):
-  #     posts_serializer = FridgeFoodSerializer(data=request.data)
-  #     if posts_serializer.is_valid():
-  #         # posts_serializer.save()
-  #         console.log("hello, received in backend")
-  #         return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
-  #     else:
-  #         print('error', posts_serializer.errors)
-  #         return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
-
-# class ReceiptView(viewsets.ModelViewSet):   
-
-def convertImageToText (image_name):
-    print(image_name)
-    img = Image.open(image_name[1:])
-    text = tess.image_to_string(img)
-    print(text)
-    return text
-
-
-class ReceiptView(APIView):
-  # print("///////////////////////")   
-  # serializer_class = ReceiptSerializer 
-  # print("2222222222222222")           
-  # queryset = Receipt.objects.all()  
-  # print("333333333333") 
-
-  parser_classes = (MultiPartParser, FormParser)
-  print("444444444444444") 
   def get(self, request, *args, **kwargs):
-      print("2222222222")
-      receipts = Receipt.objects.all()
-      serializer = ReceiptSerializer(receipts, many=True)
+      #only get expired foods in fridge
+      # return a sorted order of fridge foods, with those expiring soon at the beginning
+      fridgeFoods = FridgeFood.objects.all()
+      serializer = FridgeFoodSerializer(fridgeFoods, many=True)
+
+
       return Response(serializer.data)
 
 
+class FridgeFoodView(APIView):       
 
+  def get_object(self, pk):
+  # may not need 
+        try:
+            return FridgeFood.objects.get(pk=pk)
+        except FridgeFood.DoesNotExist:
+            raise Http404
+
+  def get(self, request, *args, **kwargs):
+  # return a sorted order of fridge foods, with those expiring soon at the beginning
+      fridgeFoods = FridgeFood.objects.all()
+      serializer = FridgeFoodSerializer(fridgeFoods, many=True)
+
+
+      return Response(serializer.data)
+
+  def put(self, request, pk, format=None):
+  # only called when an fridge food item has been removed from fridge
+    fridgeFood = self.get_object(pk)
+    serializer = FridgeFoodSerializer(fridgeFood, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def post(self, request, *args, **kwargs):
+  # add new food entry in
+  # automatically calculate the date of expiry and number of days till expiry
+      posts_serializer = FridgeFoodSerializer(data=request.data)
+      if posts_serializer.is_valid():
+          posts_serializer.save()
+          print("fridge food received in backend")
+          return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
+      else:
+          print('error', posts_serializer.errors)
+          return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+  
+  
+class ReceiptView(APIView):
+  # parser_classes = (MultiPartParser, FormParser)
+  # print("444444444444444") 
+  # def get(self, request, *args, **kwargs):
+  #     print("2222222222")
+  #     receipts = Receipt.objects.all()
+  #     serializer = ReceiptSerializer(receipts, many=True)
+  #     return Response(serializer.data)
 
   def post(self, request, *args, **kwargs):
       # print('///////////////////////')
@@ -92,6 +103,14 @@ class ReceiptView(APIView):
         return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
 
 
-class KnownExpiryDateFoodView(viewsets.ModelViewSet):       
+class KnownExpiryDateFoodView(APIView):       
   serializer_class = KnownExpiryDateFoodSerializer          
   queryset = KnownExpiryDateFood.objects.all()           
+
+  
+def convertImageToText (image_name):
+    print(image_name)
+    img = Image.open(image_name[1:])
+    text = tess.image_to_string(img)
+    print(text)
+    return text
