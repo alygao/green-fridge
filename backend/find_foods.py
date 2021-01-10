909883d5081d12767bce51aecafcc820cdd01091
Xintong Li <55467454+XintongLi1@users.noreply.github.com>
2021-01-10 05:09:09 -0500

    fixed a bug

--- backend/find_foods.py
+++ backend/find_foods.py
@@ -1,11 +1,13 @@
 
 def find_foods(strins):
-    ignore = ['receipt', 'subtotal', 'tax', '$', '\f', '.', ':', ',', '?', '#', " "] + [str(i) for i in range(10)]
+    ignore = ['receipt', 'subtotal', 'tax', '$', '\f', '.', ':', ',', '?', '#'] + [str(i) for i in range(10)]
     res = []
     for strin in strins:
         st = strin.lower()
         for i in ignore:
             st = st.replace(i, "")
+        while st != "" and st[-1] == ' ':
+            st = st[:-1]
         if st != "":
             res.append(st)
 
