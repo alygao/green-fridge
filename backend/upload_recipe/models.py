a965efd8bf2494ecc7696c3cd12302fa67b4447a
alyssagao1120 <alyssa.gao@yahoo.ca>
2021-01-09 21:18:33 -0500

    finished all APIs functions

--- backend/upload_recipe/models.py
+++ backend/upload_recipe/models.py
@@ -15,7 +15,7 @@ class FridgeFood (models.Model) :
     fridge_food_bought_date = models.DateField(default=date.today)
     fridge_food_expire_date = models.DateField(default=date.today()+timedelta(days=365)) # expire in 1 year
     days_to_expire = models.IntegerField(default=0)
-    removed_from_fridge = models.BooleanField(default=True)
+    removed_from_fridge = models.BooleanField(default=False)
 
 
     # def __str__(self):
@@ -23,12 +23,11 @@ class FridgeFood (models.Model) :
     #     + ', Expire: ' + str(self.fridge_food_expire_date) + 'Expired: ' + self.removed_from_fridge
 
     def __str__(self):
-        return self.fridge_food_name + ' will expire in ' + str(self.days_to_expire) + ' days. Bought: ' + str(self.fridge_food_bought_date) + ', Expire: ' + str(self.fridge_food_expire_date) + 'Expired: ' + str(self.removed_from_fridge)
+        return self.fridge_food_name + ' will expire in ' + str(self.days_to_expire) + ' days. Bought: ' + str(self.fridge_food_bought_date) + ', Expire: ' + str(self.fridge_food_expire_date) + ', Expired: ' + str(self.removed_from_fridge)
 
     def removeFromFridge(self):
         self.removed_from_fridge = True
 
 
 class Receipt (models.Model):
-    print("555555555555")
     file = models.FileField(upload_to="receipts/")
--- backend/upload_recipe/views.py
+++ backend/upload_recipe/views.py
@@ -14,6 +14,8 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
+import os
+
 #TODO: return the "about to expire" items
 
 # Create your views here.
@@ -106,14 +108,38 @@ class FridgeFoodView(APIView):
   # add new food entry in
   # automatically calculate the date of expiry and number of days till expiry
       print(request.data)
-      posts_serializer = FridgeFoodSerializer(data=request.data)
-      if posts_serializer.is_valid():
-          posts_serializer.save()
-          print("fridge food received in backend")
-          return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
-      else:
-          print('error', posts_serializer.errors)
-          return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+      try:
+        name = request.data['fridge_food_name']
+        print(name)
+        knownFoodInDatabase = KnownExpiryDateFood.objects.get(known_food_name = name)
+        if (knownFoodInDatabase != None) :
+            print(name)
+            expiration_days = knownFoodInDatabase.expiration_days
+            today = date.today()
+            expiry_day = today + timedelta(days=expiration_days)
+            new_fridge_food = FridgeFood.objects.create(fridge_food_name=name, fridge_food_bought_date=today, fridge_food_expire_date=expiry_day, days_to_expire=expiration_days, removed_from_fridge=False)
+            print(new_fridge_food)
+            new_fridge_food.save()
+            response = {
+              "id": new_fridge_food.id,
+              "fridge_food_name": new_fridge_food.fridge_food_name,
+              "fridge_food_bought_date": new_fridge_food.fridge_food_bought_date,
+              "fridge_food_expire_date": new_fridge_food.fridge_food_expire_date,
+              "days_to_expire": new_fridge_food.days_to_expire,
+              "removed_from_fridge": new_fridge_food.removed_from_fridge
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+      except Exception as e:
+        print(e)
+        print(name + " doesn't exist in known database")
+        return Response(status=status.HTTP_400_BAD_REQUEST)  
+
+      # posts_serializer = FridgeFoodSerializer(data=request.data)
+      # if posts_serializer.is_valid():
+      #     posts_serializer.save()
+      #     print("fridge food received in backend")
+        
+        # print('error', posts_serializer.errors)
   
   
 class ReceiptView(APIView):
@@ -135,11 +161,44 @@ class ReceiptView(APIView):
         print(request.data)
         # print('???????')
         image_address = posts_serializer.data.get('file')
+        image_address = image_address[1:]
         # print(posts_serializer.data)
         # print(temp.get('file'))
+
+        # convert image to text
         text = convertImageToText(image_address)
+        os.remove(image_address)
+
         text = text.split("\n")
         text = list(filter(None, text))
+        text = find_foods(text)
+
+        # addItemsToDatabase(text);
+
+        # def addItemsToDatabase(strings):
+        for string in text:
+          string = string.lower()
+          # print('before try')
+          try:
+            knownFoodInDatabase = KnownExpiryDateFood.objects.get(known_food_name = string)
+            if (knownFoodInDatabase != None) :
+              print(string)
+              expiration_days = knownFoodInDatabase.expiration_days
+              today = date.today()
+              expiry_day = today + timedelta(days=expiration_days)
+              new_fridge_food = FridgeFood.objects.create(fridge_food_name=string, fridge_food_bought_date=today, fridge_food_expire_date=expiry_day, days_to_expire=expiration_days, removed_from_fridge=False)
+              print(new_fridge_food)
+              new_fridge_food.save()
+            else:
+              # print('not found')
+              continue
+            # print('after if else')
+          except Exception as e:
+            print(e)
+            print(string + " doesn't exist in known database")
+
+
+
 
         context = {
           "info" : "success",
@@ -160,7 +219,21 @@ class KnownExpiryDateFoodView(APIView):
   
 def convertImageToText (image_name):
     print(image_name)
-    img = Image.open(image_name[1:])
+    img = Image.open(image_name)
     text = tess.image_to_string(img)
-    print(text)
+    # print(text)
     return text
+
+
+def find_foods(strings):
+    ignore = ['receipt', 'subtotal', 'tax', '$', '\f', '.', ':', ',', '?', '#', " "] + [str(i) for i in range(10)]
+    res = []
+    for string in strings:
+        st = string.lower()
+        for i in ignore:
+            st = st.replace(i, "")
+        if st != "":
+            res.append(st)
+
+    return res
+
